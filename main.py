b871b2bb017641b87c4fedd45833c54df7c9ff41
MichaelOli <michaeloliveira38@gmail.com>
2024-06-06 21:03:21 -0300

    Importando arquivos 1.0

--- main.py
+++ main.py
@@ -1,15 +1,32 @@
-# Importando Briblioteca
+#Importando o modulo
+from modulo import *
+import time
+#programa principal
 
-from modulo import calcular_circulo
-
-#Entrada de dados
-#b= int(input('Informe o valor da base: '))
-#h = int(input('Informe o valor da altura: '))
-
-#print(f'Area do quadrilatero: {modulo.calcular_quadrilatero(b,h)}')
-
-r= str(input('Informe o valor do Raio: ')).replace(',','.')
-
-r= float(r)
-
-print(f'Area do circulo: {calcular_circulo(r)}.')
+if __name__ == '__main__':
+    while True:
+        exibir_menu()
+        opcao = input('Opção Desejada: ')
+        match opcao:
+            case '1':
+                b = int(input('Base do quadrilatero:'))
+                h = int(input('Altura do quadrilatero:'))
+                print(f'Área do quadrlátero: {calcular_quadrilatero(b,h)}')
+                continue
+            case '2':
+                r = int(input('Raio do cículo: '))
+                print(f'Área do cículo: {calcular_circulo(r)}.')
+                continue
+            case '3':
+                b = int(input('Área do Triangulo: '))
+                h = int(input('Altura do Triangulo: '))
+                print(f'Área do Triangulo: {calcular_triagulo(b,h)}')
+                continue
+            case '4':
+                b_menor = int(input('Base menor: '))
+                b_maior = int(input('Base maior: '))
+                h = int(input('Altura do trapezio: '))
+                print(f'Area do trapezio: {calcular_trapezio(b_menor, b_maior, h)}')
+            case '5':
+                print("Saindo do programa...")
+                break
--- modulo.py
+++ modulo.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 #Função do  quadrilatero
 def calcular_quadrilatero(b,h):
     a = b* h
@@ -14,3 +16,18 @@ def calcular_triagulo(b,h):
 def calcular_trapezio(b_menor, b_maior,h):
     a = ((b_menor+b_maior)*h)/2
     return a
+# Função de Exibir o Menu
+def exibir_menu():
+    meses = ('Janeiro', 'Fevereiro', 'Março', 'Abril','Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro')
+    dia = date.today().day
+    mes = date.today().month
+    ano = date.today().year
+    print(f'{'='*6} CALCULANDO FIGURAS GEOMETRICAS {'='*6}')
+    print(f'\n{dia} de {meses[mes -1]} de {ano}.\n')
+   
+    print('1 - Calcular quadrilatero')
+    print('2 - Calcular Círculo')
+    print('3 - Calcular Triângulo')
+    print('4 - Calcular Trapezio')
+    print('5 - Sair do programa')
+
